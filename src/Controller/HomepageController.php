e111dd217d6aa2d3b566966e0a8861100db117e6
Rafael Nieto <rafael.nieto@passportscan.net>
2023-03-12 19:46:50 +0100

    first endpoint

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+use App\Service\EventService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
+    private $eventService;
+
+    public function __construct(EventService $eventService) {
+        $this->eventService = $eventService;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
     public function index(): Response
     {
+        $events = $this->eventService->getEvents();
+
         return $this->render('homepage/index.html.twig', [
             'title' => 'Welcome to my website',
             'content' => 'This is a basic homepage'
---
+++ src/Service/EventService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class EventService
+{
+    private $apiUrl;
+    private $httpClient;
+
+    public function __construct(string $apiUrl)
+    {
+        $this->apiUrl = $apiUrl;
+        $this->httpClient = HttpClient::create([
+            'verify_peer' => false,
+            'verify_host' => false,
+        ]);
+    }
+
+    public function getEvents()
+    {
+        $response = $this->httpClient->request('GET', $this->apiUrl . '/events');
+        $data = $response->toArray();
+
+        return $data['hydra:member'];
+    }
+}
